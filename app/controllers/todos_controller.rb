a68b2f894cc1f0f548434f0af8ff9cdfe6866f06
Eric Oestrich <eric@oestrich.org>
2014-09-25 10:30:03 -0400

    View a todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,9 @@ class TodosController < ApplicationController
   def index
     render :json => Todo.all, :serializer => TodosSerializer, :each_serializer => TodoSerializer
   end
+
+  def show
+    todo = Todo.find(params[:id])
+    render :json => todo, :serializer => TodoSerializer
+  end
 end
--- spec/acceptance/todos_spec.rb
+++ spec/acceptance/todos_spec.rb
@@ -4,15 +4,15 @@ require 'rspec_api_documentation/dsl'
 resource "Todos" do
   include_context :routes
 
-  get "/todos" do
-    let!(:todo) do
-      Todo.create({
-        :title => "finish my homework",
-        :due_date => Date.parse("2014-10-01"),
-        :notes => "calculus will be hard",
-      })
-    end
+  let!(:todo) do
+    Todo.create({
+      :title => "finish my homework",
+      :due_date => Date.parse("2014-10-01"),
+      :notes => "calculus will be hard",
+    })
+  end
 
+  get "/todos" do
     example_request "Viewing a list of all todos" do
       expect(response_body).to be_json_eql({
         "_embedded" => {
@@ -41,6 +41,27 @@ resource "Todos" do
             "href" => todos_url(:host => host),
           },
         }
+      }.to_json)
+    end
+  end
+
+  get "/todos/:id" do
+    let(:id) do
+      todo.id
+    end
+
+    example_request "Viewing a todo" do
+      expect(response_body).to be_json_eql({
+        "title" => "finish my homework",
+        "due_date" => "2014-10-01",
+        "notes" => "calculus will be hard",
+        "_links" => {
+          "curies" => [{
+            "name" => "todos",
+            "href" => "http://todos.smartlogic.io/relations/{rel}",
+            "templated" => true
+          }],
+        },
       }.to_json)
     end
   end
